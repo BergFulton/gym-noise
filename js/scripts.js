21b6f03b88d3739611af7ee0fcabfb6640d04a61
Tracey Berg-Fulton <traceybergfulton@gmail.com>
2020-06-24 20:02:17 -0400

    Initial play button

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,20 @@
-// If the Global Play button is clicked, then play all the sounds, at 50% vol to start
-
-// If a user wants to turn down the sound, then use the slider to set the audio
 
 
-var audios = document.querySelector('global-play-btn');
-var soundsArray = document.querySelectorAll('.slider');
-
-function playAllAudios() {
-    console.log('in playAllAudios');
-    audios.forEach(function(audio) {
+function play() {
+    const audio = document.getElementById('global-play-btn');
+    const secondarySounds = document.getElementsByClassName('secondary-btn');
+    if (audio.paused) {
         audio.play();
-    });
-};
+        [...secondarySounds].forEach(sound => {
+            sound.play();     
+        });
+    } else{
+        audio.pause();
+        audio.currentTime = 0;
+        [...secondarySounds].forEach(sound => {
+            sound.pause();     
+        });
+    }
+}
 
-soundsArray.forEach(function(elem) {
-
-    elem.addEventListener('change', function() {
-        elem.volume = this.value / 100;
-        console.log(elem.volume);
-    });
-});
-
-var btn = document.getElementById('btn');
-document.addEventListener(btn, playAllAudios, false);
-btn.click();
+//Increase & decrease volume
